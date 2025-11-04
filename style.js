d5e9ef5b58c783ab843c40cdcb50a4678da16f55
unknown <adewalesalam09@gmail.comclear>
2025-11-04 18:30:16 +0100

    commit

---
+++ style.js
@@ -0,0 +1,33 @@
+const url = "https://dummyjson.com/users";
+const fetchData = {
+  method: "GET",
+  headers: {
+    "content-type": "application/json",
+    Accept: "application/json",
+  },
+};
+
+let users = document.querySelector("#users")
+
+fetch(url, fetchData)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const response = data.users;
+    console.log(response);
+    users.innerHTML = response
+      .map(function (value) {
+        return `
+        <div class="users">
+        <img src="${value.image}" alt="">
+        <p>Name:${value.firstName}-<span>${value.lastName}</span>-<span>${value.maidenName}</span></P>
+        <p>Age:${value.age}</p>
+        <p>Date of Birth:${value.birthDate}</p>
+        <p>Gender:${value.gender}</p>
+        <p>SSN:${value.ssn}</p>
+        </div>
+        `;
+      })
+      .join("");
+  });
